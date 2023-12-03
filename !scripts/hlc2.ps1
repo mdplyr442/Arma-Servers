7767e365c07e31625c0af17b2802e5bb7521d4d5
MildlyInterested <mueller.manuel@mailbox.org>
2023-12-03 17:19:47 +0100

    sanitized HC scripts and configs

--- !scripts/hlc2.ps1
+++ !scripts/hlc2.ps1
@@ -1,6 +1,17 @@
+# Get secrets from file in key-value pairs
+$secretsFilePath = Join-Path -Path $PSScriptRoot -ChildPath "secrets.txt"
+$secrets = @{}
+Get-Content -Path $secretsFilePath | ForEach-Object {
+    # Split each line into key and value
+    $key, $value = $_ -split '=', 2
+    # Store the key-value pair in the hashtable
+    $secrets[$key] = $value
+}
+
+$joinPassword = $secrets["ARMA_CONNECT_PASSWORD"]
 $mods = (Get-ChildItem -Path F:\16AA\modpacks\main -Directory -Filter "*@*"  | Select-Object -expand fullname) -join ';'
 
-$arguments = "-client -connect=127.0.0.1 -port=2302 -password=placeholder -profiles=F:\16AA\logs_main_hc\hc2 -malloc=mimalloc_v206_LockPages -hugepages -maxMem=16000 -limitFPS=500 -enableHT -mod=$mods"
+$arguments = "-client -connect=127.0.0.1 -port=2302 -password=$joinPassword -profiles=F:\16AA\logs_main_hc\hc2 -malloc=mimalloc_v206_LockPages -hugepages -maxMem=16000 -limitFPS=500 -enableHT -mod=$mods"
 
 echo "args:" + $arguments
 
