b9351bbbbc2e2d39cf733c57dd1b12dbcb795c49
MildlyInterested <mueller.manuel@mailbox.org>
2023-12-06 20:58:00 +0100

    adjusted startup script

--- !scripts/!start_all.ps1
+++ !scripts/!start_all.ps1
@@ -1,8 +1,8 @@
 powershell -executionpolicy bypass -File main_startup.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File hlc1.ps1
+powershell -executionpolicy bypass -File hc1.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File hlc2.ps1
+powershell -executionpolicy bypass -File hc2.ps1
 Start-Sleep -Seconds 5
 powershell -executionpolicy bypass -File training_startup.ps1
 Start-Sleep -Seconds 5
---
+++ !scripts/hc2.ps1
@@ -0,0 +1,19 @@
+# Get secrets from file in key-value pairs
+$secretsFilePath = Join-Path -Path $PSScriptRoot -ChildPath "secrets.txt"
+$secrets = @{}
+Get-Content -Path $secretsFilePath | ForEach-Object {
+    $key, $value = $_ -split '=', 2
+    $secrets[$key] = $value
+}
+$ModpackPath = Join-Path -Path $PSScriptRoot -ChildPath "..\modpacks\main"
+$ProfilesPath = Join-Path -Path $PSScriptRoot -ChildPath "..\logs_main_hc\hc2"
+$ExePath = Join-Path -Path $PSScriptRoot -ChildPath "..\server_main_hc\arma3server_x64.exe"
+
+$joinPassword = $secrets["ARMA_CONNECT_PASSWORD"]
+$Mods = (Get-ChildItem -Path $ModpackPath -Directory -Filter "*@*"  | Select-Object -expand fullname) -join ';'
+
+$Arguments = "-client -connect=127.0.0.1 -port=2302 -password=$joinPassword -profiles=$ProfilesPath -malloc=mimalloc_v206_LockPages -hugepages -maxMem=16000 -limitFPS=500 -enableHT -mod=$Mods"
+
+echo "args:" + $Arguments
+
+Start-Process -FilePath "$ExePath" -ArgumentList $Arguments
