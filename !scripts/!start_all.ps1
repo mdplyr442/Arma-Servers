3eee9192bce6892ee5fea77bc42fd81b5993405e
16AA-Milsim-3LSR <16aa+3lsr@mailbox.org>
2024-01-06 20:32:31 +0000

    use path in case script is executed one folder up

--- !scripts/!start_all.ps1
+++ !scripts/!start_all.ps1
@@ -1,9 +1,9 @@
-powershell -executionpolicy bypass -File main_startup.ps1
+powershell -executionpolicy bypass -File !scripts/main_startup.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File hc1.ps1
+powershell -executionpolicy bypass -File !scripts/hc1.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File hc2.ps1
+powershell -executionpolicy bypass -File !scripts/hc2.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File training_startup.ps1
+powershell -executionpolicy bypass -File !scripts/training_startup.ps1
 Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File testing_startup.ps1
+powershell -executionpolicy bypass -File !scripts/testing_startup.ps1
