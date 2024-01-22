6f4c34c6fc582f0a8f660364968cfbf4a873d51b
MildlyInterested <mueller.manuel@mailbox.org>
2024-01-22 20:26:16 +0100

    closes https://github.com/16AA-Milsim/Arma-Servers/issues/2

--- !scripts/!start_all.ps1
+++ !scripts/!start_all.ps1
@@ -1,9 +1,16 @@
-powershell -executionpolicy bypass -File !scripts/main_startup.ps1
-Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File !scripts/hc1.ps1
-Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File !scripts/hc2.ps1
-Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File !scripts/training_startup.ps1
-Start-Sleep -Seconds 5
-powershell -executionpolicy bypass -File !scripts/testing_startup.ps1
+$ParentPath = $PSScriptRoot
+$MainPath = Join-Path -Path $ParentPath -ChildPath "main_startup.ps1"
+$TrainingPath = Join-Path -Path $ParentPath -ChildPath "training_startup.ps1"
+$TestingPath = Join-Path -Path $ParentPath -ChildPath "testing_startup.ps1"
+$Hc1Path = Join-Path -Path $ParentPath -ChildPath "hc1.ps1"
+$Hc2Path = Join-Path -Path $ParentPath -ChildPath "hc2.ps1"
+
+powershell -executionpolicy bypass -File $MainPath
+Start-Sleep -Seconds 2
+powershell -executionpolicy bypass -File $TrainingPath
+Start-Sleep -Seconds 2
+powershell -executionpolicy bypass -File $TestingPath
+Start-Sleep -Seconds 2
+powershell -executionpolicy bypass -File $Hc1Path
+Start-Sleep -Seconds 2
+powershell -executionpolicy bypass -File $Hc2Path
